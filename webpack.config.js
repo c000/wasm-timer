182481e27bbfd6e4d7276b3ed2d05701fa0a6b0c
c000 <c000@users.noreply.github.com>
2020-12-04 00:16:36 +0900

    Fixed title

--- webpack.config.js
+++ webpack.config.js
@@ -35,7 +35,8 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: "index.html",
-      inject: 'head'
+      inject: 'head',
+      title: "WASM Timer"
     }),
     new WasmPackPlugin({
       crateDirectory: __dirname,
